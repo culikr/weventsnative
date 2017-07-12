d9c2eb720e25be2e34faf617a87f24f55f65b16b
Renato Nunes <renatonunes@Renatos-MacBook-Pro.local>
2017-07-11 21:32:58 -0300

    commit com teste de funcao e gerando dados aleatorios

--- src/action/request.js
+++ src/action/request.js
@@ -1,7 +1,7 @@
  function getCityId(cityName) {
-    const APIKEY = 'gDUUNHqhBr3Fku0WU4zFy0Ffmr1AorrW';
-    const apiUrl = 'https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${APIKEY}&q=${cityName}';
-    return fetch('apiUrl')
+    const APIKEY = 'CGuGYYklWVzjsjtnPsXuMrCsCjTkqnhs';
+    const apiUrl = 'https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey='+APIKEY+'&q='+cityName+'';
+    return fetch(apiUrl)
       .then((response) => response.json())
       .then((responseJson) => {
         return responseJson.Key[0];
@@ -13,15 +13,14 @@
   
   
   function getCityInfo(key) {
-  const APIKEY = 'gDUUNHqhBr3Fku0WU4zFy0Ffmr1AorrW';
-  const apiUrlTemp = 'https://dataservice.accuweather.com/forecasts/v1/daily/1day/${key}?apikey=${APIKEY}&language=es-es&details=true&metric=true';
+  const APIKEY = 'CGuGYYklWVzjsjtnPsXuMrCsCjTkqnhs';
+  const apiUrlTemp = `https://dataservice.accuweather.com/forecasts/v1/daily/1day/${key}?apikey=${APIKEY}&language=es-es&details=true&metric=true`
+    return fetch(apiUrlTemp)
     .then((response) => response.json())
-      .then((responseJson) => {
+    .then((responseJson) => {
         return responseJson;
       })
       .catch((error) => {
         console.error(error);
       });
   }
-  
-}
